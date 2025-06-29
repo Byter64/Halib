a3fe881b9f45fd6fd79d6506dcc412a7e606e808
Byter64 <byter64@gmx.de>
2025-06-29 12:40:42 +0200

    Added Font::SetSize

--- include/Halib/Graphic/Font.h
+++ include/Halib/Graphic/Font.h
@@ -8,8 +8,13 @@ namespace Halib
 	{
 		//This comes from freetype
 		FT_Face face;
+		const char* path;
 
 public:
 		Font(const char* path, int face = 0);
+		
+		/// @brief Sets the size of the font. The result might not be exactly the given target size. See freetypes documentation for more
+		/// @param targetPixelSize
+		void SetSize(int targetPixelSize);
 	};
 } // namespace Halib
--- source/Graphic/Font.cpp
+++ source/Graphic/Font.cpp
@@ -5,6 +5,7 @@ FT_Library freetypeLibrary;
 
 Halib::Font::Font(const char* path, int face)
 {
+	this->path = path;
 	int error = FT_New_Face(freetypeLibrary, path, face, &this->face);
 	if (error == FT_Err_Unknown_File_Format)
 	{
@@ -15,5 +16,17 @@ Halib::Font::Font(const char* path, int face)
 	{
 	  printf("Could not load font: %s\n", path);
 	  printf("Errorcode: %i\n", error);
+	}
+}
+
+void Halib::Font::SetSize(int targetPixelSize)
+{
+	int error = FT_Set_Pixel_Sizes(face, targetPixelSize, targetPixelSize);
+	if(error)
+	{
+		printf("SetSize did not work\n");
+		printf("Asset: %s\n", path);
+		printf("Attempted size: %i\n", targetPixelSize);
+		printf("Errorcode: %i\n", error);
 	}
 }
