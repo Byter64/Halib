b590753f36574af9c8e9d081e6e7331f1a13b500
Byter64 <byter64@gmx.de>
2025-07-02 08:58:06 +0200

    Added: GetButton functions Added: UpdateInputs function

--- include/Halib/System.h
+++ include/Halib/System.h
@@ -2,13 +2,50 @@
 
 namespace Halib
 {
+	enum Button
+	{
+		A,
+		B,
+		X,
+		Y, 
+		START,
+		SELECT,
+		UP,
+		DOWN,
+		LEFT,
+		RIGHT,
+		LEFT_SHOULDER,
+		RIGHT_SHOULDER
+	};
+
 	void Init();
+
+	void UpdateInputs();
 
 	//Desktop: true, after the window has been requested to close
 	//wueHans: always false
 	bool GetShouldClose();
 
 	/// @brief 
-		/// @return Time since startup in seconds
-		float GetTimeSinceStartup();
+	/// @return Time since startup in seconds
+	float GetTimeSinceStartup();
+
+	/// @brief 
+	/// @param controllerID Must be 0 or 1 
+	/// @param button 
+	/// @return True, if the button is currently held down
+	bool GetButtonDown(int controllerID, Button button);
+	
+	/// @brief 
+	/// @param controllerID Must be 0 or 1
+	/// @param button 
+	/// @return True, if the button was just pressed down
+	bool GetButtonPressed(int controllerID, Button button);
+	
+	/// @brief 
+	/// @param controllerID Must be 0 or 1
+	/// @param button 
+	/// @return True, if the button was just released
+	bool GetButtonReleased(int controllerID, Button button);
+
 } // namespace Halib
--- source/System.cpp
+++ source/System.cpp
@@ -8,6 +8,12 @@
 //Defined in Font.cpp
 extern FT_Library freetypeLibrary;
 
+static const int CONTROLLER_COUNT = 2;
+static Hall::ControllerState oldState[CONTROLLER_COUNT];
+static Hall::ControllerState newState[CONTROLLER_COUNT];
+static Hall::ControllerState pressedState[CONTROLLER_COUNT];
+static Hall::ControllerState releasedState[CONTROLLER_COUNT];
+
 void Halib::Init()
 {
 	Hall::Init();
@@ -20,6 +26,17 @@ void Halib::Init()
 	}
 }
 
+void Halib::UpdateInputs()
+{
+	for(int i = 0; i < CONTROLLER_COUNT; i++)
+	{
+		oldState[i] = newState[i];
+		newState[i] = Hall::GetController(i);
+		pressedState[i] = ~oldState[i] & newState[i];
+		releasedState[i] = oldState[i] & ~newState[i];
+	}
+}
+
 bool Halib::GetShouldClose()
 {
 	return Hall::ShouldClose();
@@ -28,4 +45,61 @@ bool Halib::GetShouldClose()
 float Halib::GetTimeSinceStartup()
 {
 	return Hall::GetSystemTime() / (float)Hall::SYSTEM_CLK_FREQUENCY;
+}
+
+bool Halib::GetButtonDown(int controllerID, Button button)
+{
+	switch (button) 
+	{
+	case A: return Hall::GetA(newState[controllerID]);
+	case B: return Hall::GetB(newState[controllerID]);
+	case X: return Hall::GetX(newState[controllerID]);
+	case Y: return Hall::GetY(newState[controllerID]);
+	case LEFT: return Hall::GetLeft(newState[controllerID]);
+	case RIGHT: return Hall::GetRight(newState[controllerID]);
+	case UP: return Hall::GetUp(newState[controllerID]);
+	case DOWN: return Hall::GetDown(newState[controllerID]);
+	case START: return Hall::GetStart(newState[controllerID]);
+	case SELECT: return Hall::GetSelect(newState[controllerID]);
+	case LEFT_SHOULDER: return Hall::GetL(newState[controllerID]);
+	case RIGHT_SHOULDER: return Hall::GetR(newState[controllerID]);
+	}
+}
+
+bool Halib::GetButtonPressed(int controllerID, Button button)
+{
+	switch (button) 
+	{
+	case A: return Hall::GetA(pressedState[controllerID]);
+	case B: return Hall::GetB(pressedState[controllerID]);
+	case X: return Hall::GetX(pressedState[controllerID]);
+	case Y: return Hall::GetY(pressedState[controllerID]);
+	case LEFT: return Hall::GetLeft(pressedState[controllerID]);
+	case RIGHT: return Hall::GetRight(pressedState[controllerID]);
+	case UP: return Hall::GetUp(pressedState[controllerID]);
+	case DOWN: return Hall::GetDown(pressedState[controllerID]);
+	case START: return Hall::GetStart(pressedState[controllerID]);
+	case SELECT: return Hall::GetSelect(pressedState[controllerID]);
+	case LEFT_SHOULDER: return Hall::GetL(pressedState[controllerID]);
+	case RIGHT_SHOULDER: return Hall::GetR(pressedState[controllerID]);
+	}
+}
+
+bool Halib::GetButtonReleased(int controllerID, Button button)
+{
+	switch (button) 
+	{
+	case A: return Hall::GetA(releasedState[controllerID]);
+	case B: return Hall::GetB(releasedState[controllerID]);
+	case X: return Hall::GetX(releasedState[controllerID]);
+	case Y: return Hall::GetY(releasedState[controllerID]);
+	case LEFT: return Hall::GetLeft(releasedState[controllerID]);
+	case RIGHT: return Hall::GetRight(releasedState[controllerID]);
+	case UP: return Hall::GetUp(releasedState[controllerID]);
+	case DOWN: return Hall::GetDown(releasedState[controllerID]);
+	case START: return Hall::GetStart(releasedState[controllerID]);
+	case SELECT: return Hall::GetSelect(releasedState[controllerID]);
+	case LEFT_SHOULDER: return Hall::GetL(releasedState[controllerID]);
+	case RIGHT_SHOULDER: return Hall::GetR(releasedState[controllerID]);
+	}
 }
