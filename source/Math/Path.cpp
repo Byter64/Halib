b07202754b44710b2358f0e4a5f0d21449527603
Yannik <byter64@gmx.de>
2025-07-12 08:27:43 +0200

    Fixed bug in path

--- source/Math/Path.cpp
+++ source/Math/Path.cpp
@@ -8,9 +8,9 @@ Halib::Path::Path(std::vector<Vec2> points)
 
 Halib::Vec2 Halib::Path::GetPosition(float x)
 {
-	x = x > points.size() ? points.size() : x;
+	x = x >= (points.size() - 1) ? (points.size() - 1) : x;
 	x = x < 0 ? 0 : x;
-	int indexA = x == points.size() ? x - 1 : x;
+	int indexA = x == (points.size() - 1) ? x - 1 : x;
 	int indexB = indexA + 1;
 
 	Vec2 a = points[indexA];
