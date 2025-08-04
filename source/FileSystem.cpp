bd54f190c7288fd130465cdbe8ae20ec511ee309
Byter <byetr64@gmx.de>
2025-08-04 10:05:21 +0200

    Added defines to LOGS and LOG again

--- source/FileSystem.cpp
+++ source/FileSystem.cpp
@@ -4,9 +4,13 @@ namespace Engine::Files
 {
 #ifdef DESKTOP
     static const std::filesystem::path ASSETS = "Assets";
+    static const std::filesystem::path LOGS = "Logs";
+    static const std::filesystem::path LOG = "Logs/Log.txt";
 #endif
 
 #ifdef WUEHANS
     static const std::filesystem::path ASSETS = "/Assets";
+    static const std::filesystem::path LOGS = "/Logs";
+    static const std::filesystem::path LOG = "/Logs/Log.txt";
 #endif
 }
